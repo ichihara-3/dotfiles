d99d5de3ca4a67e1283928a178d0bb904225c43e
ichi_taro3 <taro3.ichi@gmail.com>
2019-10-13 13:22:11 +0900

    added my zshell settigs

---
+++ .my.zsh
@@ -0,0 +1,10 @@
+export EDITOR=vim
+export PATH="${PATH}:${HOME}/go/bin"
+
+# FZF configurations
+export FZF_DEFAULT_COMMAND="find . -type d -name '.git' -prune -o -type f -name '*.pyc' -prune -o -type f -print"
+# To apply the command to CTRL-T as well
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+export FZF_DEFAULT_OPTS="--height 40% --reverse --border"
+export FZF_CTRL_T_OPTS="--height 40% --reverse --border --preview 'cat {}'"
