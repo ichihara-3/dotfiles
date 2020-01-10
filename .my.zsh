7b56bd514a967620406c10cc7d0682b9f16ae853
ichi_taro3 <taro3.ichi@gmail.com>
2020-01-10 22:27:57 +0900

    add alias of prezto theme select command

--- .my.zsh
+++ .my.zsh
@@ -8,3 +8,5 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 export FZF_DEFAULT_OPTS="--height 40% --reverse --border"
 export FZF_CTRL_T_OPTS="--height 40% --reverse --border --preview 'cat {}'"
+
+alias pn='prompt -s $(prompt -l |tail -n 1 | tr " " "\n" | fzf)'
