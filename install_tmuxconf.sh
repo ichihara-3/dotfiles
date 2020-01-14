6436e07b2937cfb9e173721c000b516dae661a22
ichi_taro3 <taro3.ichi@gmail.com>
2020-01-15 00:07:28 +0900

    script for installing .tmux.conf

---
+++ install_tmuxconf.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DIR=$(cd $(dirname $0); pwd)/tmux
+echo ${DIR}
+
+if tmux -V > /dev/null 2>&1; then
+  if [[ -e ~/.tmux.conf ]]; then
+    echo backup: mv ~/.tmux.conf ~/.tmux.conf.bk
+    mv ~/.tmux.conf ~/.tmux.conf.bk
+  fi
+  echo install: ln -sf ${DIR}/.tmux.conf ~/.tmux.conf
+  ln -sf ${DIR}/.tmux.conf ~/.tmux.conf
+else
+  echo tmux not exists...
+fi
+
+echo finished!
