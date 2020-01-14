61e3d8137e35f19212df49d73789033ec7afd22b
ichi_taro3 <taro3.ichi@gmail.com>
2020-01-15 00:07:45 +0900

    check if vimrc exists

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -3,9 +3,11 @@ DIR=$(cd $(dirname $0); pwd)/vim
 echo $DIR
 
 if vim --version > /dev/null 2>&1; then
-  echo backup: mv ~/.vimrc ~/.vimrc.bk
-  mv ~/.vimrc ~/.vimrc.bk
-  echo install: ln -s ${DIR}/.vimrc ~/.vimrc
+  if [[ -e ~/.vimrc ]]; then
+    echo backup: mv ~/.vimrc ~/.vimrc.bk
+    mv ~/.vimrc ~/.vimrc.bk
+  fi
+  echo install: ln -sf ${DIR}/.vimrc ~/.vimrc
   ln -sf ${DIR}/.vimrc ~/.vimrc
 else
   echo vim not exists...
