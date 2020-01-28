7bed42708fc65978283d9126cb26de6f23798014
ichi_taro3 <taro3.ichi@gmail.com>
2020-01-28 22:43:34 +0900

    changed vimrc name

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -8,7 +8,7 @@ if vim --version > /dev/null 2>&1; then
     mv ~/.vimrc ~/.vimrc.bk
   fi
   echo install: ln -sf ${DIR}/.vimrc ~/.vimrc
-  ln -sf ${DIR}/.vimrc ~/.vimrc
+  ln -sf ${DIR}/vimrc.vim ~/.vimrc
 else
   echo vim not exists...
 fi
