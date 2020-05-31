d90ea0a67766bfb4ee37afe6c1add8323f5d0ad7
ichi_taro3 <taro3.ichi@gmail.com>
2020-05-31 22:04:18 +0900

    add GOPATH environmental variables

--- .my.zsh
+++ .my.zsh
@@ -17,3 +17,5 @@ prompt powerlevel10k
 
 # heroku autocomplete setup
 HEROKU_AC_ZSH_SETUP_PATH=/Users/kotaro/Library/Caches/heroku/autocomplete/zsh_setup && test -f $HEROKU_AC_ZSH_SETUP_PATH && source $HEROKU_AC_ZSH_SETUP_PATH;
+
+export GOPATH=~/go/
